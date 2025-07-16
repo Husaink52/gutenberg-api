922f85ac05abc5b60e21e49940faeefb5d35974a
Husaink52 <112974649+Husaink52@users.noreply.github.com>
2025-07-16 16:45:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Update the password and user if needed
-DATABASE_URL = "mysql://root:vtuflswOKSDWDZkVytNDeqSEQWpgEWEV@interchange.proxy.rlwy.net:23585/railway"
+DATABASE_URL = "mysql+pymysql://root:vtuflswOKSDWDZkVytNDeqSEQWpgEWEV@interchange.proxy.rlwy.net:23585/railway"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
